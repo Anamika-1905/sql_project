8be3e5834159934168635d525fd59438eefdc9fa
Anamika-1905 <Anamikapandey703@gmail.com>
2025-11-04 01:15:53 +0530

    Delete Corporate project directory

--- Corporate project/Corporate_ Between & And operator.sql
+++
@@ -1,4 +0,0 @@
-select	* 
-from corporate
-where Date_of_birth between '1971-08-17' and '1987-11-04'
-and Salary <= 250000;
--- Corporate project/Corporate_and operator.sql
+++
@@ -1,2 +0,0 @@
-select * from corporate
-where salary > 100000 and salary < 200000;
