602c8f5961df98384567f29034766f94fd1eaa13
Anamika-1905 <Anamikapandey703@gmail.com>
2025-11-04 01:15:31 +0530

    Delete Cinema directory

--- Cinema/Cinema_And operator.sql
+++
@@ -1,2 +0,0 @@
-select * from cinema
-where rating >= 6.5 and	Rating <= 8.5 and Decription = 'fiction'
--- Cinema/Cinema_Table.sql
+++
@@ -1,7 +0,0 @@
-Create table Cinema
-(
-Movie_ID int not null,
-Movie_Name varchar(150),
-Decription varchar(150),
-Rating decimal(5,1)
-);
